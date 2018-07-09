2bba5318a8b427b602999e919a70fe5e00f400c8
Frederik Ring <frederik.ring@gmail.com>
2018-07-09 17:08:22 +0200

    add library code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # rasa-mongo-tracker-store
-TrackerStore for rasa_core connecting to MongoDB
+> `TrackerStore` for rasa_core connecting to MongoDB
+
+## Usage
+
+`MongoTrackerStore` can be used when loading or instantiating an Agent:
+
+```py
+from rasa_mongo_tracker_store.store import MongoTrackerStore
+
+agent = Agent.load(
+    'path/to/dialogue/models',
+    tracker_store=MongoTrackerStore(
+        None, # rasa's internal logic will set the domain lateron
+        host='localhost', port=27017, collection_name='rasa_data'
+    ))
+
+```
+
+### License
+MIT © [Frederik Ring](http://www.frederikring.com)
+
---
+++ rasa_mongo_tracker_store/store.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+from rasa_core.tracker_store import TrackerStore
+
+
+class MongoTrackerStore(TrackerStore):
+    """
+    MongoTrackerStore stores rasa conversation trackers using MongoDB
+    """
+    def __init__(self, domain, collection_name='rasa_tracker_store', host=None,
+                 port=None, tz_aware=None, connect=None):
+
+        m_client = MongoClient(host=host, port=port,
+                               tz_aware=tz_aware, connect=connect)
+        self.collection = m_client[collection_name]
+        super(MongoTrackerStore, self).__init__(domain)
+
+    def save(self, tracker):
+        serialised_tracker = self.serialise_tracker(tracker)
+        self.collection.update_one(
+            {'sender_id': tracker.sender_id},
+            {'$set': {'sender_id': tracker.sender_id, 'tracker': serialised_tracker}},
+            upsert=True)
+
+    def retrieve(self, sender_id):
+        stored = self.collection.find_one({'sender_id': sender_id})
+        if stored is not None:
+            return self.deserialise_tracker(sender_id, stored['tracker'])
+        return None
+
+    def keys(self):
+        pass
